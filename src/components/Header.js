f69e7dcf2d8821b9f2d282cefe2da928951c2cfe
Preety Thakur <76190720+preety92@users.noreply.github.com>
2023-09-20 12:23:01 -0400

    updated header ,footer and loader

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="header">
+        <h1>Disaster Map </h1>
+        <a className="nasa-header" href="https://eonet.sci.gsfc.nasa.gov/" target="_blank" rel="noreferrer">data by NASA</a>
+      </header>
+    );
+  };
+  
+  export default Header;
